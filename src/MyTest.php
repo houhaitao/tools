b0a7cde6f9c707b773925c8dcecf2f408a2d005d
无聊小刚 <haitao_hou@126.com>
2015-12-09 13:53:05 +0800

    测试

---
+++ src/MyTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hthou
+ * Date: 2015/12/9
+ * Time: 13:20
+ */
+namespace Tools;
+
+class MyTest
+{
+    public function test()
+    {
+        echo "sdfsdf";
+    }
+}
